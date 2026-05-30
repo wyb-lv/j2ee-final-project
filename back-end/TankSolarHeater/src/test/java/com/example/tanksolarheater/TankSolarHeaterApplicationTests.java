de0cc4c261765b3c7647a2433c04c5aa36b4c123
Vũ Bảo Huy <vubaohuy2903@gmail.com>
2026-05-30 18:45:10 +0700

    initial commit

---
+++ back-end/TankSolarHeater/src/test/java/com/example/tanksolarheater/TankSolarHeaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tanksolarheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankSolarHeaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
